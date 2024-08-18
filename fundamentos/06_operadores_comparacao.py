218e41c027902c3309efcb8dfba47791da48091c
escuderodev <escudeiro.ti@gmail.com>
2024-08-17 23:08:58 -0300

    feat: operadores de atribuição

---
+++ fundamentos/06_operadores_comparacao.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 10
+num3 = str(num1)
+num4 = 5
+
+print(num1 == num2)
+print(num1 > num4)
+print(num1 < num4)
+print(num1 >= num2)
+print(num4 != num1)
+print(num1 == num3)
+
---
+++ fundamentos/07_operadores_atribuicao.py
@@ -0,0 +1,12 @@
+saldo = 1000
+print(saldo)
+saldo += 500
+print(saldo)
+saldo -= 250
+print(saldo)
+saldo += 1
+print(saldo)
+saldo *= 2
+print(saldo)
+saldo /= 2
+print(saldo)
