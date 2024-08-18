3d819733d359999e29cf9d3bab2986d2db5b93cf
escuderodev <escudeiro.ti@gmail.com>
2024-08-17 22:43:30 -0300

    feat: operadores aritiméticos

---
+++ fundamentos/05_operadores_aritmeticos.py
@@ -0,0 +1,14 @@
+# soma
+print(10 + 3)
+# subtração
+print(10 - 3)
+# multiplicação
+print(10 * 3)
+# divisão
+print(10 / 3)
+# divisão mantendo número inteiro
+print(10 // 3)
+# resto da divisão
+print(10 % 3)
+# exponenciação
+print(10 ** 3)
