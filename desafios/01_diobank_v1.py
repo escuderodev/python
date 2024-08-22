10409903eaacb29f4ae736a56f6c29a5dd2c67dd
escuderodev <escuderodev@outlook.com>
2024-08-22 00:34:58 -0300

    feat: project diobank finished

--- desafios/01_diobank_v1.py
+++ desafios/01_diobank_v1.py
@@ -1,6 +1,7 @@
 saldo = 0
 limite_por_saque = 500.00
 soma_dos_saques_realizados_no_dia = 0
+LIMITE_DE_SAQUES_POR_DIA = 3
 extrato = []
 
 def depositar(valor):
@@ -15,15 +16,20 @@ def sacar(valor):
     global limite_por_saque
     global soma_dos_saques_realizados_no_dia
     global extrato
+    global LIMITE_DE_SAQUES_POR_DIA
     if valor > limite_por_saque:
-        print(f"O valor R$ {valor:.2f} excede o limite permitido por saque!")
+        print(f"O valor R$ {valor:.2f} excede o limite permitido por saque. Saque não realizado!")
+    elif LIMITE_DE_SAQUES_POR_DIA == 0:
+        print("Você já atingiu o limite de saques diários que é 3. Saque não realizado!")
     elif soma_dos_saques_realizados_no_dia > 1000.00:
         print("O valor solicitado excede o limite diário permitido para saques. Saque não realizado!")
     elif valor <= saldo:
         saldo -= valor
         soma_dos_saques_realizados_no_dia += valor
+        LIMITE_DE_SAQUES_POR_DIA -= 1
         extrato.append(f"- R$ {valor:.2f}")
         print(f"Saque no valor de R$ {valor:.2f} realizado com sucesso!")
+        print(f"Saldo atual R$ {saldo:.2f}")
     else:
         print(f"Saldo insuficiente!")
 
@@ -32,17 +38,25 @@ def gera_extrato():
     for transacao in extrato:
         print(f"{transacao}")
     print(f"Saldo atual R$: {saldo:.2f}")
+    
+def exibir_menu():
+    print("\n=== Bem vindo ao DioBank ===")
+    
+    while True:
+        opcao = input("\nDigite D para Depósito, S para Saque, E para Extrato ou Q para Sair: ")
         
-
-print("\n=== Depositando Valor ===")
-depositar(2000.00)
-
-print("\n=== Sacando Valor ===")
-sacar(500.00)
-sacar(501.00)
-sacar(500.00)
-sacar(500.00)
-sacar(1.00)
-
-print("\n=== Gerando Extrato ===")
-gera_extrato()
+        if opcao.upper() == "D":
+            valor = int(input("Digite o valor do depósito R$: "))
+            depositar(valor)
+        elif opcao.upper() == "S":
+            valor = int(input("Digite o valor do saque R$: "))
+            sacar(valor)
+        elif opcao.upper() == "E":
+            gera_extrato()
+        elif opcao.upper() == "Q":
+            print("Encerrando o sistema!")
+            break
+        else:
+            print("Opção inválida!")
+            
+exibir_menu()
