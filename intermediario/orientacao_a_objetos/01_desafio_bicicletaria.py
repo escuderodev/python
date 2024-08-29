bf2669d09a46ec4fe3ee8a79475e55b36aa4a648
escuderodev <escudero.ti@gmail.com>
2024-08-29 19:27:05 -0300

    feat: criando a classe bibileta

---
+++ intermediario/orientacao_a_objetos/01_desafio_bicicletaria.py
@@ -0,0 +1,44 @@
+class Bicicleta:
+    def __init__(self, cor: str, modelo: str, ano: int, valor: float):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = int(ano)
+        self.valor = float(valor)
+        
+    def detalhar(self):
+        print(f"\nModelo: {self.modelo} | Cor: {self.cor} | Ano de Fabricação: {self.ano} | Valor R$: {self.valor:.2f}")
+        
+    def businar(self):
+        print("Bicileta businando...")
+    
+    def parar(self):
+        print("Bicileta parando...")
+    
+    def correr(self):
+        print("Bicileta acelerando...")
+        
+    def get_cor(self):
+        return self.cor
+    
+    def get_modelo(self):
+        return self.modelo
+    
+    def get_ano(self):
+        return self.ano
+    
+    def get_valor(self):
+        return self.valor
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+        
+speed = Bicicleta("Preta", "Speed", 2024, 2500.00)
+speed.detalhar()
+speed.businar()
+speed.correr()
+speed.parar()
+print(speed.get_modelo())
+print(speed.get_cor())
+print(speed.get_ano())
+print(f"{speed.get_valor():.2f}")
+print(speed.__str__())
