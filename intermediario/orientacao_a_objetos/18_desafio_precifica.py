eecf72b59c858787c227430ea7df6bb370f2004e
escuderodev <escudero.ti@gmail.com>
2024-09-05 21:16:10 -0300

    feat: desafio precifica

--- intermediario/orientacao_a_objetos/18_desafio_precifica.py
+++ intermediario/orientacao_a_objetos/18_desafio_precifica.py
@@ -33,17 +33,38 @@ class Insumo:
 class ProdutoInsumo:
     def __init__(self, nome_insumo: str, valor_unitario: float, quantidade: int):
         self.nome_insumo = nome_insumo
-        self.valor_unitatio = valor_unitario
+        self.valor_unitario = valor_unitario
         self.quantidade = quantidade
 
 class Produto:
     def __init__(self, nome: str):
         self._nome = nome
-        self._insumos = []
+        self.insumos = []
+        self._valor = 0.00
 
     @property
     def nome(self):
         return self._nome
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def adicionar_insumo(self, produto_insumo):
+        self.insumos.append({
+            "nome_insumo": produto_insumo.nome_insumo,
+            "valor_unitario": produto_insumo.valor_unitario,
+            "quantidade": produto_insumo.quantidade
+        })
+        valor_insumo = produto_insumo.valor_unitario * produto_insumo.quantidade
+        self._valor += valor_insumo
+
+    def __str__(self):
+        return f"""
+        Produto: {self.nome}
+        Insumos: {self.insumos}
+        Valor R$: {self._valor:.2f}
+        """
 
 xilitol = Insumo("Xilitol",500, 70.00)
 cacau = Insumo("Cacau 100%", 1000, 50.00)
@@ -51,3 +72,15 @@ leite_sem_lactose = Insumo("Leite sem Lactose", 1000, 5.70)
 print(xilitol)
 print(cacau)
 print(leite_sem_lactose)
+
+produto_insumo1 = ProdutoInsumo(xilitol.nome, xilitol.valor_unitario, 25)
+produto_insumo2 = ProdutoInsumo(cacau.nome, cacau.valor_unitario, 350)
+produto_insumo3 = ProdutoInsumo(leite_sem_lactose.nome, leite_sem_lactose.valor_unitario, 1000)
+
+produto = Produto("Brigadeiro 100% Cacau")
+
+produto.adicionar_insumo(produto_insumo1)
+produto.adicionar_insumo(produto_insumo2)
+produto.adicionar_insumo(produto_insumo3)
+
+print(produto)
