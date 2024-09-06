8a8254f44191d9414d2cfa05cb19534bd0122230
escuderodev <escudero.ti@gmail.com>
2024-09-05 21:51:31 -0300

    feat: método detalhar_produto criado

--- intermediario/orientacao_a_objetos/18_desafio_precifica.py
+++ intermediario/orientacao_a_objetos/18_desafio_precifica.py
@@ -41,6 +41,7 @@ class Produto:
         self._nome = nome
         self.insumos = []
         self._valor = 0.00
+        self._preco_sugerido = 0.00
 
     @property
     def nome(self):
@@ -56,22 +57,30 @@ class Produto:
             "valor_unitario": produto_insumo.valor_unitario,
             "quantidade": produto_insumo.quantidade
         })
+
         valor_insumo = produto_insumo.valor_unitario * produto_insumo.quantidade
         self._valor += valor_insumo
+        self._preco_sugerido += (self.valor + (self._valor * 0.70))
+
+    def detalhar_produto(self):
+        print("========== Produto ==========")
+        for insumo in produto.insumos:
+            print(f"Nome do Insumo: {insumo['nome_insumo']}")
+            print(f"Valor Unitário: R$ {insumo['valor_unitario']:.2f}")
+            print(f"Quantidade: {insumo['quantidade']}\n")
+        print(f"====== Valor R$: {self.valor:.2f} ======")
 
     def __str__(self):
         return f"""
         Produto: {self.nome}
         Insumos: {self.insumos}
-        Valor R$: {self._valor:.2f}
+        Preço de Custa R$: {self._valor:.2f}
+        Preço Sugerido R$: {self._preco_sugerido:.2f}
         """
 
 xilitol = Insumo("Xilitol",500, 70.00)
 cacau = Insumo("Cacau 100%", 1000, 50.00)
 leite_sem_lactose = Insumo("Leite sem Lactose", 1000, 5.70)
-print(xilitol)
-print(cacau)
-print(leite_sem_lactose)
 
 produto_insumo1 = ProdutoInsumo(xilitol.nome, xilitol.valor_unitario, 25)
 produto_insumo2 = ProdutoInsumo(cacau.nome, cacau.valor_unitario, 350)
@@ -84,3 +93,5 @@ produto.adicionar_insumo(produto_insumo2)
 produto.adicionar_insumo(produto_insumo3)
 
 print(produto)
+
+produto.detalhar_produto()
