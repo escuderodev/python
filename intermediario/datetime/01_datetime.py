0d4ec6c11d20617de794fc01c751e4ae5691fda0
escuderodev <escuderodev@outlook.com>
2024-09-20 19:11:43 -0300

    feat: trabalhando com datetime

---
+++ intermediario/datetime/01_datetime.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime
+
+my_date = date(2023, 7, 19)
+print(my_date)
+
+my_datetime = datetime.now()
+print(my_datetime)
+
+print(date.today())
+print(datetime.today())
+
+my_data_e_hora = datetime(1984, 4, 26, 23, 57, 00)
+print(my_data_e_hora)
