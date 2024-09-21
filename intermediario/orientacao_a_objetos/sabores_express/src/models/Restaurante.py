f32ee9025e9d68609fdf973681bca8877913dd16
escuderodev <escuderodev@outlook.com>
2024-09-21 13:50:41 -0300

    feat: projeto concluído

--- intermediario/orientacao_a_objetos/sabores_express/src/models/Restaurante.py
+++ intermediario/orientacao_a_objetos/sabores_express/src/models/Restaurante.py
@@ -1,48 +1,41 @@
+from models.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
-    
-    def __init__(self, nome: str, categoria: str):
+
+    def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
-        self.restaurantes.append(self)
-        
+        self._avaliacao = []
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
+    
     @classmethod
     def listar_restaurantes(cls):
-        print("===== Restaurantes =====")
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}')
         for restaurante in cls.restaurantes:
-            print(restaurante)
-            
-    def alterar_situacao_restaurante(self):
-        if(self._ativo == False):
-            self._ativo = True
-        else:
-            self._ativo = False
-            
-    @property
-    def nome(self):
-        return self._nome
-
-    @property
-    def categoria(self):
-        return self._categoria
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
     @property
     def ativo(self):
-        return self._ativo
+        return '⌧' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-    def __str__(self):
-        return f"Nome: {self._nome.ljust(25)} | Categoria: {self._categoria.ljust(25)} | Situação: {self._ativo}"
-        
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota <= 5: 
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
 
-restaurante = Restaurante("praza san lorenzo", "Restaurante")
-restaurante_mcdonalds = Restaurante("MCDonalds", "Fast Food")
-
-print()
-Restaurante.listar_restaurantes()
-
-restaurante.alterar_situacao_restaurante()
-Restaurante.listar_restaurantes()
-
-restaurante.alterar_situacao_restaurante()
-Restaurante.listar_restaurantes()
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
