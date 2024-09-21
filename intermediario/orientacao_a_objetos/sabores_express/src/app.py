e15aae4bd19385038f2a761b88bbd6ccd5609f73
escuderodev <escuderodev@outlook.com>
2024-09-21 14:35:53 -0300

    feat: methods add_item implementeds

--- intermediario/orientacao_a_objetos/sabores_express/src/app.py
+++ intermediario/orientacao_a_objetos/sabores_express/src/app.py
@@ -2,15 +2,20 @@ from models.restaurante import Restaurante
 from models.prato import Prato
 from models.bebida import Bebida
 
+# instanciando objetos
 restaurante = Restaurante('praça san lorenzo', 'Gourmet')
 prato = Prato("Parmeggiana de Carne", 80.00, "Parmeggiana de Carne coberto com Queijo Mussarela e Molho de Tomate.")
-bebida = Bebida("Coca Cola", 7.50, 0.350)
+bebida = Bebida("Coca Cola", 7.50, 350, "ML")
 
+# adicionando itens ao cardápio
+restaurante.adicionar_bebida_ao_cardapio(bebida)
+restaurante.adicionar_prato_ao_cardapio(prato)
+
+# executando a aplicação
 def main():
     print()
     print(restaurante)
-    print(prato)
-    print(bebida)
+    restaurante.listar_cardapio()
 
 if __name__ == '__main__':
     main()
--- intermediario/orientacao_a_objetos/sabores_express/src/models/bebida.py
+++ intermediario/orientacao_a_objetos/sabores_express/src/models/bebida.py
@@ -1,13 +1,18 @@
 from models.item_cardapio import ItemCardapio
 
 class Bebida(ItemCardapio):
-    def __init__(self, nome: str, preco: float, tamanho: float):
+    def __init__(self, nome: str, preco: float, tamanho: int, unidade_medida: str):
         super().__init__(nome, preco)
         self._tamanho = tamanho
+        self._unidade_medida = unidade_medida
         
     @property
     def tamanho(self):
         return self._tamanho    
     
+    @property
+    def unidade_medida(self):
+        return self._unidade_medida
+    
     def __str__(self) -> str:
-        return f'Bebida: {self._nome} - Tamanho: {self._tamanho} - R$: {self._preco:.2f}'
+        return f'Bebida: {self._nome} - Tamanho: {self._tamanho}{self._unidade_medida} - R$: {self._preco:.2f}'
